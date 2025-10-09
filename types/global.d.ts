d04060b06902d277c42d3ecc5d2c1850a33acd73
Serhii <dumok001@gmail.com>
2025-10-09 18:19:16 +0300

    refactor: add self and importScripts to global scope definitions

--- types/global.d.ts
+++ types/global.d.ts
@@ -9,6 +9,9 @@ declare global {
 	var document: any;
 	/** Universal global object - available in both browser and Node.js */
 	var globalThis: any;
+	var self: DedicatedWorkerGlobalScope;
+	
+	function importScripts(...urls: string[]): void;
 }
 
 // This export statement makes this file a module
